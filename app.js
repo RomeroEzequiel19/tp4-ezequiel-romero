c7f8136f1ae96678898ee202efa00bec1949857f
Carlos Ezequiel Romero <ezequielromero3039@gmail.com>
2023-06-13 13:36:47 -0300

    Configuracion de los middlewares helmet, morgan, cors y express.json()

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require("express");
-const app = express();
+const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('morgan');
 
+//Inicializo la libreria express
+const app = express();
 const port = 6000;
+
+
+
+// Configuración de Middlewares
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+
 
 // Ejecución del servidor
 app.listen(port, () =>
